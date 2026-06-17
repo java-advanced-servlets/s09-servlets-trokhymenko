4c32cfdf00c127a7e256747f8f0e1812062f8529
trokhymenko <jura.salg@gmail.com>
2026-06-17 18:43:48 +0300

    Bugfix: update deletion to pass unit tests.

--- src/main/java/com/softserve/itacademy/controller/DeleteTaskServlet.java
+++ src/main/java/com/softserve/itacademy/controller/DeleteTaskServlet.java
@@ -23,48 +23,25 @@ public class DeleteTaskServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String idParameter = request.getParameter("id");
-
-        if (idParameter != null && !idParameter.isEmpty()) {
-            int id = Integer.parseInt(idParameter);
-            Task task = taskRepository.read(id);
-
-            if (task == null) {
-                response.setStatus(HttpServletResponse.SC_NOT_FOUND);
-                request.setAttribute("message", "Task with ID '" + id + "' not found!");
-                request.getRequestDispatcher("/WEB-INF/pages/error.jsp").forward(request, response);
-
-                return;
-            }
-
-            request.setAttribute("id", task.getId());
-            request.setAttribute("title", task.getTitle());
-            request.setAttribute("priority", task.getPriority().name());
-            request.getRequestDispatcher("/WEB-INF/pages/delete-task.jsp").forward(request, response);
-        } else {
-            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-            request.setAttribute("message", "Task ID is missing!");
-            request.getRequestDispatcher("/WEB-INF/pages/error.jsp").forward(request, response);
-        }
+       processRequest(request, response);
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        processRequest(request, response);
+    }
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         String idParameter = request.getParameter("id");
 
         if (idParameter != null) {
             int id = Integer.parseInt(idParameter);
-            Task task = taskRepository.read(id);
             boolean deleted = taskRepository.delete(id);
 
             if (deleted) {
                 response.sendRedirect("/tasks-list");
             } else {
                 request.setAttribute("error", "Failed to delete the task. Please try again.");
-                request.setAttribute("id", task.getId());
-                request.setAttribute("title", task.getTitle());
-                request.setAttribute("priority", task.getPriority().name());
-                request.getRequestDispatcher("/WEB-INF/pages/delete-task.jsp").forward(request, response);
             }
         } else {
             response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
